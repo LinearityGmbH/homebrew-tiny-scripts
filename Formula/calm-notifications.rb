63c76523df2c81a8bf0af2b21d021b6d1b0f4e02
Sam Wyndham <samw@linearity.io>
2022-06-07 16:59:11 +0200

    WIP

--- Formula/calm-notifications.rb
+++ Formula/calm-notifications.rb
@@ -1,7 +1,7 @@
 class CalmNotifications < Formula
   desc "Enable, disable, toggle, and show status of Do Not Disturb on macOS Big Sur"
   homepage "https://github.com/vitorgalvao/tiny-scripts"
-  url "https://github.com/LinearityGmbH/tiny-scripts.git"
+  url "https://github.com/vitorgalvao/tiny-scripts.git", :using => :git, :revision => '2d9277e96981906ccad1c5e270f54e1e6c6f9d81'
   version "2021.2"
 
   def install
